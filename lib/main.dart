55d70dad256962cc10a05bdeab04471b4e6aee92
nicolasukd <nicolas.jorquera@sansano.usm.cl>
2023-05-12 08:36:48 -0400

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 import 'src/app.dart';
 
-
-
 // void main() => runApp(const App());
-void main() => runApp(
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+
       // DevicePreview(
       //   enabled: !kReleaseMode,
       //   builder: (context) => const App(), // Wrap your app
       // ),
-    const App()
-    );
+      const App());
+}
